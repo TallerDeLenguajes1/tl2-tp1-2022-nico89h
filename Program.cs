a1b8184887bd094cb7bb3adf4dff02a7bf21a17f
Your Name <you@example.com>
2022-09-20 19:11:41 -0300

    otro commit

--- Program.cs
+++ Program.cs
@@ -1,91 +1,60 @@
-﻿using System.IO;
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Net;
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-// See https://aka.ms/new-console-template for more information
-/*
-Problema 1:
-Realizar una aplicación de consola que cargue de un número entero por teclado e
-imprimir su cuadrado
-*/
-// System.Console.WriteLine("Hola pibeeee, dame un numero para que lo eleve al cuadrado xfa");
-// int x= Int32.Parse(Console.ReadLine());
-// //aca la elevo al cuadrado a la piba
-// System.Console.WriteLine("El numero ingresado elevado al cuadrado es:{0}", Math.Pow(x,2));
-
-/*
-Inicio de el problema 2
-Realizar una aplicación de consola que cargue 2 números y haga la división entre ellos.
-// */
-// System.Console.WriteLine("Dame 2 numeros para dividir");
-// System.Console.WriteLine("Numero 1");
-// float X= float.Parse(Console.ReadLine());
-// System.Console.WriteLine("Numero 2");
-// float Y= float.Parse(Console.ReadLine());
-// System.Console.WriteLine("La division entre ellos es: {0}", (X/Y));
-/*
-Problema 3:
-Cree una aplicación que ingrese kilómetros conducidos y litros usados, y calcule
-kilómetros por litro
+﻿/*
+) Implementar la solución del siguiente programa usando Try Catch para capturar los posibles
+errores.
+Escribir la declaración de una clase que almacene información de un empleado: Datos
+personales, Dirección, fecha de ingreso la empresa, datos profesionales.
+a) La clase empleado deberá calcular la antigüedad en la empresa, la edad del
+empleado y el salario. Este se calcula de acuerdo a la fórmula: Salario = Sueldo
+Básico + Adicional – Descuento
+Descuento = 15 % del Sueldo Básico.
+Adicional = 1 % del sueldo básico por cada año de antigüedad
+2 años_________________________ 2%
+15 años_________________________ 1 5%
+> 20 años________________________ 25% (a partir de 20 años se fija en 25%)
+b) Modifica luego la clase de tal manera que puedas suministrar la información:
+i) Si es casado, la cantidad de hijos.
+ii) Si es divorciado, la fecha de su divorcio.
+iii) Si tiene título universitario, el título y la universidad que impartió el título.
+c) Permite el ingreso de N trabajadores
+d) Muestra en una lista los datos: Apellido y Nombre, Edad, Antigüedad y Salario de los
+trabajadores ingresados
 */
 
-// System.Console.WriteLine("Dame el kilometro y los litros usados para calcular km por litro");
-// System.Console.WriteLine("Kilometro");
-// float X= float.Parse(Console.ReadLine());
-// System.Console.WriteLine("Litro");
-// float Y= float.Parse(Console.ReadLine());
-// System.Console.WriteLine("Los litros utilizados por km son: {0}", (X/Y));
-/*
-Problema 4:
-Realizar una aplicación de consola que consuma la siguiente api
-https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre y que muestre el
-listado de provincias y su correspondiente id.
-*/
-
-
-//inicio de la api
-var url="https://apis.datos.gob.ar/georef/api/provincias?campos=id,nombre"; //datos de los elixirs de harry potter
-var request=(HttpWebRequest)WebRequest.Create(url);
-request.Method="GET";
-request.ContentType="application/json";
-request.Accept="application/json";
-//el inicio de la api
-try
+//Inicio d eel cargo
+enum Cargos
 {
-    using (WebResponse response=request.GetResponse())
-    {
-        using (Stream strReader = response.GetResponseStream())
-        {
-            if (strReader==null) return;
-            using (StreamReader objReader= new StreamReader(strReader))
-            {
-                string respondeBody = objReader.ReadToEnd();
-
-                var provincias= JsonSerializer.Deserialize<List<Provincia>>(respondeBody);
-                
-                //inicio de el control de el elixir
-                int l = 0;
-                for (int p = 0; p < provincias.Count; p++) // recorro los datos de la lista recibida de la api
-                {
-                    System.Console.WriteLine("Las provincia es: "+ provincias[p].Nombre+ "Su id es: "+ provincias[p].Id);
-                }
-            }
-        }
-    }
+    Ingeniero, 
+    Medico,
+    Programador,
+    Psicologo
 }
-catch{
-    System.Console.WriteLine("Se preodujo un error");
+//Inicio de la clase
+class Empleado{
+    //inicio de la declaracion de los atributos
+    private string nombre, apellido, direccion;
+    private long  Dni;
+    private DateTime fechaIngreso;
+    private float sueldo, sueldoBasico;
+    private Cargos tipoEmpleo;
+    private int antiguedad;
+    public Empleado(string Nombre, string Apellido, string Direccion, long DNI, DateTime FechaIngreso, float SueldoBasico, Cargos TipoEmpleo){
+        this.nombre=Nombre;
+        this.apellido=Apellido;
+        this.Dni=DNI;
+        this.direccion=Direccion;
+        this.fechaIngreso=FechaIngreso;
+        this.sueldoBasico=SueldoBasico;
+        this.tipoEmpleo=TipoEmpleo;
+        this.antiguedad=  DateTime.Now.Year-this.fechaIngreso.Year; //obtengo la antiguedad
+        this.sueldo= this.sueldoBasico;//hacer la cuenta
+    }    
 }
-
-
-
-public class Provincia
-{
-    [JsonPropertyName("id")]
-    public string Id { get; set; }
-
-    [JsonPropertyName("nombre")]
-    public string Nombre { get; set; }
-}
+/*
+Este se calcula de acuerdo a la fórmula: Salario = Sueldo
+Básico + Adicional – Descuento
+Descuento = 15 % del Sueldo Básico.
+Adicional = 1 % del sueldo básico por cada año de antigüedad
+2 años_________________________ 2%
+15 años_________________________ 1 5%
+> 20 años________________________ 25% (a partir de 20 años se fija en 25%)
+*/
